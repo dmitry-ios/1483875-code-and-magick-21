d4ae8c9d63ef6e70bd89b5902c841ec24d5d3d04
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-10-03 18:52:38 +0300

    Добавляет изменение внешнего вида и оживляет диалоговое окно с валидацией поля

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const TOO_SHORT_MESSAGE = `Имя персонажа не может содержать менее 2 символов`;
+const TOO_LONG_MESSAGE = `Максимальная длина имени персонажа — 25 символов`;
+const VALUE_MISSING_MESSAGE = `Обязательное поле`;
+
 const FIRST_NAMES = [
   `Иван`,
   `Хуан Себастьян`,
@@ -39,6 +43,14 @@ const EYES_COLORS = [
   `green`
 ];
 
+const FIREBALL_COLORS = [
+  `#ee4830`,
+  `#30a8ee`,
+  `#5ce6c0`,
+  `#e848d5`,
+  `#e6e848`
+];
+
 const NUMBER_OF_WIZRDS = 4;
 
 const randomNumber = function (limit) {
@@ -55,10 +67,14 @@ const generateWizardName = function () {
   return (!isReversed) ? `${firstName} ${lastName}` : `${lastName} ${firstName}`;
 };
 
+const getRandomColor = function (colors) {
+  return colors[randomNumber(colors.length)];
+};
+
 const generateWizardData = function () {
   const name = generateWizardName();
-  const coatColor = COAT_COLORS[randomNumber(COAT_COLORS.length)];
-  const eyesColor = EYES_COLORS[randomNumber(EYES_COLORS.length)];
+  const coatColor = getRandomColor(COAT_COLORS);
+  const eyesColor = getRandomColor(EYES_COLORS);
 
   return {name, coatColor, eyesColor};
 };
@@ -97,13 +113,15 @@ const makeFragmetWithWizards = function (wizards) {
   return fragment;
 };
 
+const userDialog = document.querySelector(`.setup`);
+const setupOpen = document.querySelector(`.setup-open`);
+const setupClose = userDialog.querySelector(`.setup-close`);
+const setupOpenIcon = document.querySelector(`.setup-open-icon`);
+const setupUserName = document.querySelector(`.setup-user-name`);
+
 const showSetupDialog = function () {
   // Создаем волшебников со случайными параметрами
   const wizards = generateWizards(NUMBER_OF_WIZRDS);
-
-  // Показываем диалоговое окно игрока с настройками персонажа
-  const userDialog = document.querySelector(`.setup`);
-  userDialog.classList.remove(`hidden`);
 
   // Заполняем шаблонную разметку случайными персонажами
   const fragmentWithWizards = makeFragmetWithWizards(wizards);
@@ -115,3 +133,92 @@ const showSetupDialog = function () {
 };
 
 showSetupDialog();
+
+// Установка открытия и закрытия диалогового окна
+
+const onDialogEscapePress = function (evt) {
+  if (evt.key === `Escape` && evt.target !== setupUserName) {
+    closeDialog();
+  }
+};
+
+const onDialogEnterPress = function (evt) {
+  if (evt.key === `Enter`) {
+    closeDialog();
+  }
+};
+
+const onDialogCloseClick = function () {
+  closeDialog();
+};
+
+const openDialog = function () {
+  userDialog.classList.remove(`hidden`);
+
+  setupClose.addEventListener(`click`, onDialogCloseClick);
+  setupClose.addEventListener(`keydown`, onDialogEnterPress);
+
+  document.addEventListener(`keydown`, onDialogEscapePress);
+};
+
+const closeDialog = function () {
+  userDialog.classList.add(`hidden`);
+
+  document.removeEventListener(`keydown`, onDialogEscapePress);
+
+  setupClose.removeEventListener(`keydown`, onDialogEnterPress);
+  setupClose.removeEventListener(`click`, onDialogCloseClick);
+};
+
+setupOpen.addEventListener(`click`, function () {
+  openDialog();
+});
+
+setupOpenIcon.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    openDialog();
+  }
+});
+
+// Валидация поля имени пользователя
+
+setupUserName.addEventListener(`invalid`, function () {
+  const validity = setupUserName.validity;
+
+  if (validity.tooShort) {
+    setupUserName.setCustomValidity(TOO_SHORT_MESSAGE);
+  } else if (validity.tooLong) {
+    setupUserName.setCustomValidity(TOO_LONG_MESSAGE);
+  } else if (validity.valueMissing) {
+    setupUserName.setCustomValidity(VALUE_MISSING_MESSAGE);
+  } else {
+    setupUserName.setCustomValidity(``);
+  }
+});
+
+// Установка действий на изменение внешнего вида
+
+const wizardCoat = userDialog.querySelector(`.setup-wizard .wizard-coat`);
+const coatColorInput = userDialog.querySelector(`input[name=coat-color]`);
+const wizardEyes = userDialog.querySelector(`.setup-wizard .wizard-eyes`);
+const eyesColorInput = userDialog.querySelector(`input[name=eyes-color]`);
+const fireball = userDialog.querySelector(`.setup-fireball-wrap`);
+const fireballColorInput = userDialog.querySelector(`input[name=fireball-color]`);
+
+const setColorValue = function (colors, input, element, keyStyle) {
+  const color = getRandomColor(colors);
+  input.value = color;
+  element.style[keyStyle] = color;
+};
+
+wizardCoat.addEventListener(`click`, function () {
+  setColorValue(COAT_COLORS, coatColorInput, wizardCoat, `fill`);
+});
+
+wizardEyes.addEventListener(`click`, function () {
+  setColorValue(EYES_COLORS, eyesColorInput, wizardEyes, `fill`);
+});
+
+fireball.addEventListener(`click`, function () {
+  setColorValue(FIREBALL_COLORS, fireballColorInput, fireball, `backgroundColor`);
+});
