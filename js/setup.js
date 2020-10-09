2b6e1c5abf221fddad584c43dda60e83d848f84b
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-10-09 21:31:11 +0300

    Получает и отправляет данные на сервер

--- js/setup.js
+++ js/setup.js
@@ -4,15 +4,14 @@
   const NUMBER_OF_WIZRDS = 4;
 
   const userDialog = document.querySelector(`.setup`);
-
-  const wizards = window.mock.generateWizards(NUMBER_OF_WIZRDS);
+  const form = userDialog.querySelector(`.setup-wizard-form`);
 
   const renderWizard = function (wizard, templateItem) {
     const wizardElement = templateItem.cloneNode(true);
 
     wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
-    wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
-    wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
+    wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.colorCoat;
+    wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
@@ -21,8 +20,9 @@
     const similarWizardTemplate = document.querySelector(`#similar-wizard-template`);
     const similarWizardItem = similarWizardTemplate.content.querySelector(`.setup-similar-item`);
     const fragment = document.createDocumentFragment();
+    const length = Math.min(NUMBER_OF_WIZRDS, elements.length);
 
-    for (let i = 0; i < elements.length; i++) {
+    for (let i = 0; i < length; i++) {
       const wizardElement = renderWizard(elements[i], similarWizardItem);
 
       fragment.appendChild(wizardElement);
@@ -31,7 +31,7 @@
     return fragment;
   };
 
-  const setupWizardDialog = function () {
+  const setupWizardDialog = function (wizards) {
     const fragmentWithWizards = makeFragmetWizards(wizards);
     const similarListElement = userDialog.querySelector(`.setup-similar-list`);
 
@@ -40,5 +40,40 @@
     userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
   };
 
-  setupWizardDialog();
+  const successLoadHandler = function (jsonData) {
+    window.util.shuffleArray(jsonData);
+    setupWizardDialog(jsonData);
+  };
+
+  const successSaveHandler = function () {
+    userDialog.classList.add(`hidden`);
+  };
+
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+
+    window.scrollTo(0, 0);
+
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  const onFormSubmit = function (evt) {
+    const data = new FormData(form);
+
+    window.backend.save(data, successSaveHandler, errorHandler);
+
+    evt.preventDefault();
+  };
+
+  window.backend.load(successLoadHandler, errorHandler);
+
+  form.addEventListener(`submit`, onFormSubmit);
 })();
--- js/util.js
+++ js/util.js
@@ -9,8 +9,18 @@
     return colors[randomNumber(colors.length)];
   };
 
+  const shuffleArray = function (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const t = array[i];
+      array[i] = array[j];
+      array[j] = t;
+    }
+  };
+
   window.util = {
     randomNumber,
-    getRandomColor
+    getRandomColor,
+    shuffleArray
   };
 })();
