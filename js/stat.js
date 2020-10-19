fe870be2f27e198cf8d9f6da72a4ffecde2fc050
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-10-19 08:58:10 +0300

    Делает сборку с помощью webpack

--- js/stat.js
+++ js/stat.js
@@ -1,107 +1,105 @@
 'use strict';
 
-(function () {
-  const GAP = 10;
+const GAP = 10;
 
-  const CLOUD_WIDTH = 420;
-  const CLOUD_HEIGHT = 270;
-  const CLOUD_X = 100;
-  const CLOUD_Y = 10;
-  const CLOUD_COLOR = `#ffffff`;
-  const CLOUD_SHADOW = `rgba(0, 0, 0, 0.7)`;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const CLOUD_COLOR = `#ffffff`;
+const CLOUD_SHADOW = `rgba(0, 0, 0, 0.7)`;
 
-  const BAR_GAP = 50;
-  const BAR_HEIGHT = 150;
-  const BAR_WIDTH = 40;
+const BAR_GAP = 50;
+const BAR_HEIGHT = 150;
+const BAR_WIDTH = 40;
 
-  const TEXT_HEIGHT = 18;
-  const TEXT_X = CLOUD_X + GAP;
-  const TEXT_Y = CLOUD_Y + GAP;
-  const TEXT_GAP = 15;
-  const TEXT_COLOR = `#000000`;
+const TEXT_HEIGHT = 18;
+const TEXT_X = CLOUD_X + GAP;
+const TEXT_Y = CLOUD_Y + GAP;
+const TEXT_GAP = 15;
+const TEXT_COLOR = `#000000`;
 
-  const renderCloud = function (ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  };
+const renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
-  const getMaxElement = function (arr) {
-    let maxElement = arr[0];
+const getMaxElement = function (arr) {
+  let maxElement = arr[0];
 
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > maxElement) {
-        maxElement = arr[i];
-      }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
     }
+  }
 
-    return maxElement;
-  };
+  return maxElement;
+};
 
-  const getColorByPlayer = function (player) {
-    if (player === `Вы`) {
-      return `rgba(255, 0, 0, 1)`;
-    } else {
-      const saturation = Math.floor(Math.random() * 100);
-      return `hsl(240, ${saturation}%, 50%)`;
-    }
-  };
+const getColorByPlayer = function (player) {
+  if (player === `Вы`) {
+    return `rgba(255, 0, 0, 1)`;
+  } else {
+    const saturation = Math.floor(Math.random() * 100);
+    return `hsl(240, ${saturation}%, 50%)`;
+  }
+};
 
-  const drawString = function (ctx, str, x, y) {
-    str.split(`\n`).forEach(function (line, index) {
-      ctx.fillText(line, x, y + index * TEXT_HEIGHT);
-    });
-  };
+const drawString = function (ctx, str, x, y) {
+  str.split(`\n`).forEach(function (line, index) {
+    ctx.fillText(line, x, y + index * TEXT_HEIGHT);
+  });
+};
 
-  const setDefaultFont = function (ctx) {
-    ctx.font = `16px PT Mono`;
-    ctx.textBaseline = `hanging`;
-    ctx.fillStyle = TEXT_COLOR;
-  };
+const setDefaultFont = function (ctx) {
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
+  ctx.fillStyle = TEXT_COLOR;
+};
 
-  const drawInfo = function (ctx, text, x, y) {
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(text, x, y);
-  };
+const drawInfo = function (ctx, text, x, y) {
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.fillText(text, x, y);
+};
 
-  const drawBarForPlayer = function (ctx, player, x, y, height) {
-    ctx.fillStyle = getColorByPlayer(player);
-    ctx.fillRect(x, y, BAR_WIDTH, height);
-  };
+const drawBarForPlayer = function (ctx, player, x, y, height) {
+  ctx.fillStyle = getColorByPlayer(player);
+  ctx.fillRect(x, y, BAR_WIDTH, height);
+};
 
-  window.renderStatistics = function (ctx, players, times) {
-    // Показывает облако с тенью
-    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW);
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+window.renderStatistics = function (ctx, players, times) {
+  // Показывает облако с тенью
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-    // Показываем многострочный заголовок
-    setDefaultFont(ctx);
-    drawString(
-        ctx,
-        `Ура вы победили!\nСписок результатов:`,
-        TEXT_X + TEXT_GAP,
-        TEXT_Y + TEXT_GAP
-    );
+  // Показываем многострочный заголовок
+  setDefaultFont(ctx);
+  drawString(
+      ctx,
+      `Ура вы победили!\nСписок результатов:`,
+      TEXT_X + TEXT_GAP,
+      TEXT_Y + TEXT_GAP
+  );
 
-    const maxTime = getMaxElement(times);
-    const barBottom = CLOUD_Y + CLOUD_HEIGHT;
+  const maxTime = getMaxElement(times);
+  const barBottom = CLOUD_Y + CLOUD_HEIGHT;
 
-    // Рисуем статистику в виде гистограммы
-    for (let i = 0; i < players.length; i++) {
-      const currentTime = Math.round(times[i]);
-      const currentBarX = CLOUD_X + GAP + 2 * TEXT_GAP + i * (BAR_WIDTH + BAR_GAP);
-      const currentBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
-      const currentBarY = barBottom - 2.5 * TEXT_GAP - currentBarHeight;
-      const currentPointTextY = barBottom - 3.5 * TEXT_GAP - currentBarHeight;
-      const currentPlayerTextY = barBottom - 2 * TEXT_GAP;
+  // Рисуем статистику в виде гистограммы
+  for (let i = 0; i < players.length; i++) {
+    const currentTime = Math.round(times[i]);
+    const currentBarX = CLOUD_X + GAP + 2 * TEXT_GAP + i * (BAR_WIDTH + BAR_GAP);
+    const currentBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    const currentBarY = barBottom - 2.5 * TEXT_GAP - currentBarHeight;
+    const currentPointTextY = barBottom - 3.5 * TEXT_GAP - currentBarHeight;
+    const currentPlayerTextY = barBottom - 2 * TEXT_GAP;
 
-      // Показываем время игрока над столбиком
-      drawInfo(ctx, currentTime, currentBarX, currentPointTextY);
+    // Показываем время игрока над столбиком
+    drawInfo(ctx, currentTime, currentBarX, currentPointTextY);
 
-      // Рисуем столбик
-      drawBarForPlayer(ctx, players[i], currentBarX, currentBarY, currentBarHeight);
+    // Рисуем столбик
+    drawBarForPlayer(ctx, players[i], currentBarX, currentBarY, currentBarHeight);
 
-      // Показываем имя игрока под столбиком
-      drawInfo(ctx, players[i], currentBarX, currentPlayerTextY);
-    }
-  };
-})();
+    // Показываем имя игрока под столбиком
+    drawInfo(ctx, players[i], currentBarX, currentPlayerTextY);
+  }
+};
--- js/util.js
+++ js/util.js
@@ -1,43 +1,42 @@
 'use strict';
 
-(function () {
-  const randomNumber = function (limit) {
-    return Math.floor(Math.random() * limit);
-  };
+const randomNumber = function (limit) {
+  return Math.floor(Math.random() * limit);
+};
 
-  const getRandomColor = function (colors) {
-    return colors[randomNumber(colors.length)];
-  };
+const getRandomColor = function (colors) {
+  return colors[randomNumber(colors.length)];
+};
 
-  const shuffleArray = function (array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * i);
-      const t = array[i];
-      array[i] = array[j];
-      array[j] = t;
-    }
-  };
+const shuffleArray = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const t = array[i];
+    array[i] = array[j];
+    array[j] = t;
+  }
+};
 
-  const createErrorMessage = function (message) {
-    const node = document.createElement(`div`);
+const createErrorMessage = function (message) {
+  const node = document.createElement(`div`);
 
-    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
-    node.style.position = `absolute`;
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = `30px`;
+  node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+  node.style.position = `absolute`;
+  node.style.left = 0;
+  node.style.right = 0;
+  node.style.fontSize = `30px`;
 
-    node.textContent = message;
+  node.textContent = message;
 
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 
-    document.body.insertAdjacentElement(`afterbegin`, node);
-  };
+  document.body.insertAdjacentElement(`afterbegin`, node);
+};
 
-  window.util = {
-    randomNumber,
-    getRandomColor,
-    shuffleArray,
-    createErrorMessage
-  };
-})();
+window.util = {
+  randomNumber,
+  getRandomColor,
+  shuffleArray,
+  createErrorMessage
+};
+
